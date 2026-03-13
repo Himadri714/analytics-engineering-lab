7dde6d2f3cecfcefbdb273b8a29ec44fffce5597
Himadri Sarma <himadris714@gmail.com>
2026-03-13 20:10:08 +0530

    updated fields

--- ae_lab/models/marts/globepay_transaction_event_daily.sql
+++ ae_lab/models/marts/globepay_transaction_event_daily.sql
@@ -7,14 +7,11 @@ SELECT
     t.status,
     t.payment_source,
     t.transaction_date,
-    t.state,
+    t.payment_status,
     t.country,
     t.amount,
     t.currency,
     t.chargeback,
-    t.is_accepted,
-    t.is_declined,
-    t.missing_chargeback,
     (t.amount / c.conversion_rate) AS amount_in_usd
 FROM {{ ref('globepay_transaction_events') }} AS t
 INNER JOIN {{ ref('base_currency_conversion') }} AS c
